36791170f7863b75dbd8f8c1424fa2656f107b79
straussz <straussz@sheridancollege.ca>
2021-03-09 14:36:05 -0500

    Create Room Transition Prototype
    Create scripts and basic pieces for room transitioning system

---
+++ Design Week Winter 2021/Assets/RT_MoveBasic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RT_MoveBasic : MonoBehaviour
+{
+    public float speed;
+    public static RT_MoveBasic instance = null;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = 0;
+
+        gameObject.transform.position = new Vector2(transform.position.x + (h * speed),
+           transform.position.y + (v * speed));
+    }
+}
---
+++ Design Week Winter 2021/Assets/RT_RoomFlipper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RT_RoomFlipper : MonoBehaviour
+{
+    public Transform teleportLocation;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (other.gameObject.tag == "Player")
+            {
+
+                RT_MoveBasic.instance.transform.position = teleportLocation.position;
+            }
+        }
+    }
+}
